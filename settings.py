16e172067674e5e4dc486ed8469359d3914b4473
Ya095 <egor.yakovlev.95@gmail.com>
2023-08-19 23:21:46 +0300

    re-uploading to the new repo

---
+++ settings.py
@@ -0,0 +1,3 @@
+REAL_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
+
+TEST_DATABASE_URL = "postgresql+asyncpg://postgres_test:postgres_test@0.0.0.0:5433/postgres_test"
