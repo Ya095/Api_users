85d5ab307bd6a0290f6f782260dc43ecd5702c81
Ya095 <egor.yakovlev.95@gmail.com>
2023-08-23 13:43:14 +0300

    add logic give/remove admin role to user

--- api/login_handler.py
+++ api/login_handler.py
@@ -5,7 +5,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 import settings
 from api.actions.auth import authenticate_user, get_current_user_from_token
-from api.models import Token
+from api.schemas import Token
 from db.models import User
 from db.session import get_db
 from security import create_access_token
--- db/dals.py
+++ db/dals.py
@@ -1,15 +1,8 @@
-from enum import Enum
 from typing import Union
 from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import update, and_, select
-from db.models import User
-
-
-class PortalRole(str, Enum):
-    ROLE_PORTAL_USER = "ROLE_PORTAL_USER"
-    ROLE_PORTAL_ADMIN = "ROLE_PORTAL_ADMIN"
-    ROLE_PORTAL_SUPERADMIN = "ROLE_PORTAL_SUPERADMIN"
+from db.models import User, PortalRole
 
 
 class UserDAL:
